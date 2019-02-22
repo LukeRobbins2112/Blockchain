2696ba456d7254584fef4e0d376dda033e780606
LukeRobbins2112 <lukerobbins2112@gmail.com>
2019-02-22 16:00:56 -0600

    Initial commit

---
+++ Blockchain.java
@@ -0,0 +1,42 @@
+
+
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+
+
+// **********************************************************************************
+// This runs the individual processes that participate in the Blockchain
+// **********************************************************************************
+class BlockchainMember{
+
+    public BlockchainMember(){
+
+    }
+
+    void createUnverifiedBlock(){
+
+    }
+
+    void publishUnverifiedBlock(){
+
+    }
+
+}
+
+// **********************************************************************************
+// The main, coordinating process which manages each of the 
+// **********************************************************************************
+
+public class Blockchain{
+
+
+
+    public static void main(String[] args){
+
+    }
+
+}
